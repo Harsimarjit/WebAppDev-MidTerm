27dcb609a33232132c0c98c12524bad6368939e4
Harsimarjit <simarbadding@gmail.com>
2022-10-30 14:00:07 -0400

    First Commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI" : "mongodb+srv://simarmid:CzluVqKQo12qW8Xz@cluster0.svgerej.mongodb.net/?retryWrites=true&w=majority"
+};
